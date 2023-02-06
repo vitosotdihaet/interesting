07268ea87b533e7fda2caf0701ae42b77c7dc89a
vitosotdihaet <vitostikitos@gmail.com>
2023-02-06 19:50:46 +0300

    Initial commit

---
+++ updating-terminal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char table[] = "abcdefghijklmnop";
+    int W = 50, H = 20;
+    for (;;) {
+        for (int y = 0; y < H; ++y) {
+            for (int x = 0; x < W; ++x) {
+                putc(table[(x + y) % (sizeof(table) - 1)], stdout);
+            }
+            putc('\n', stdout);
+        } 
+        printf("\033[%dA", H);
+        printf("\033[%dD", W);
+    }
+
+    return 0;
+}
